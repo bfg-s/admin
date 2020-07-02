7b05cc13c9da935f0406e92a6f86e5ea828e5e01
xsaven <gms.sw.xsaven@gmail.com>
2020-07-02 19:23:47 +0300

    Changed: src/Segments/Tagable/Fields/Select.php

--- src/Segments/Tagable/Fields/Select.php
+++ src/Segments/Tagable/Fields/Select.php
@@ -3,6 +3,7 @@
 namespace Lar\LteAdmin\Segments\Tagable\Fields;
 
 use Illuminate\Contracts\Support\Arrayable;
+use Lar\Developer\Core\Select2;
 use Lar\LteAdmin\Segments\Tagable\Cores\CoreSelect2;
 use Lar\LteAdmin\Segments\Tagable\FormGroup;
 
@@ -35,10 +36,42 @@ class Select extends FormGroup
     ];
 
     /**
+     * @var mixed
+     */
+    protected $load_subject;
+
+    /**
+     * @var string
+     */
+    protected $load_format;
+
+    /**
+     * @var bool
+     */
+    protected $nullable = false;
+
+    /**
      * @return \Lar\Layout\Abstracts\Component|\Lar\Layout\Tags\INPUT|mixed
      */
     public function field()
     {
+        if ($this->load_subject) {
+            $selector = new Select2(
+                $this->load_subject,
+                $this->load_format,
+                $this->value,
+                $this->nullable ? $this->title : null,
+                $this->name . '_'
+            );
+
+            $r_name = $selector->getName();
+
+            if (request()->has($r_name)) { exit($selector->toJson(JSON_UNESCAPED_UNICODE)); }
+
+            $this->on_load('select2::ajax', $r_name)
+                ->options($selector->getValueData(), true);
+        }
+
         return CoreSelect2::create($this->options, [
             'name' => $this->name,
             'data-placeholder' => $this->title
@@ -68,5 +101,29 @@ class Select extends FormGroup
         }
 
         return $this;
+    }
+
+    /**
+     * @param $subject
+     * @param  string  $format
+     * @return $this
+     */
+    public function load($subject, string $format = null)
+    {
+        $this->load_subject = $subject;
+        $this->load_format = $format;
+
+        return $this;
+    }
+
+    /**
+     * @param  string|null  $message
+     * @return Select
+     */
+    public function nullable(string $message = null)
+    {
+        $this->nullable = true;
+
+        return parent::nullable($message);
     }
 }
