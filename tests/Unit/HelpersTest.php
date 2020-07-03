1e0c06a5d97f08ce921f64cad04a4daa4e450092
xsaven <gms.sw.xsaven@gmail.com>
2020-07-04 00:15:38 +0300

    Changed: views/access_denied.blade.php

---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lar\LteAdmin\Tests\Unit;
+
+use Lar\LteAdmin\Tests\TestCase;
+
+/**
+ * Class HelpersTest
+ * @package Lar\LteAdmin\Tests\Unit
+ */
+class HelpersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(false);
+    }
+}
--- views/access_denied.blade.php
+++
@@ -1,7 +0,0 @@
-@extends($default_page)
-
-@section('content')
-    @alertdanger('lte.error', 'fas fa-exclamation-triangle')
-        @lang('lte.access_denied')
-    @endalert
-@endsection
