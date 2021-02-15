aca5cb9ef4aff052fb18801606889d607371b7d9
xsaven <gms.sw.xsaven@gmail.com>
2021-02-15 13:34:57 +0200

    assets/js/app.js, javascript/Executors/ckeditor.js

--- javascript/Executors/ckeditor.js
+++ javascript/Executors/ckeditor.js
@@ -26,6 +26,21 @@ module.exports = class extends Executor {
             }
         };
 
+        $options.image = {
+            // You need to configure the image toolbar, too, so it uses the new style buttons.
+            toolbar: [ 'imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight' ],
+            styles: [
+                // This option is equal to a situation where no style is applied.
+                'full',
+
+                // This represents an image aligned to the left.
+                'alignLeft',
+
+                // This represents an image aligned to the right.
+                'alignRight'
+            ]
+        }
+
         return ClassicEditor.create( this.target,  $options).then((editor) => {
 
 
