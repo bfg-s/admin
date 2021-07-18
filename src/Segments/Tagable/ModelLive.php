95b79160d105cd58656c8ab68b8adedede8758e6
xsaven <gms.sw.xsaven@gmail.com>
2021-07-18 11:32:40 +0300

    src/Segments/Tagable/ModelLive.php

--- src/Segments/Tagable/ModelLive.php
+++ src/Segments/Tagable/ModelLive.php
@@ -8,6 +8,16 @@ namespace Lar\LteAdmin\Segments\Tagable;
  */
 class ModelLive extends Live {
 
+    const NOT_NULL = "NOT_NULL";
+    const NULL = "NULL";
+    const TRUE = "TRUE";
+    const FALSE = "FALSE";
+
+    /**
+     * @var mixed|null
+     */
+    public $value = null;
+
     /**
      * Live constructor.
      * @param ...$params
@@ -24,13 +34,55 @@ class ModelLive extends Live {
 
         $model = Form::$current_model;
 
-        $request_value = request($path);
+        $request_value = multi_dot_call(request()->all(), $path);
 
-        $value = old($path, ($model ? (multi_dot_call($model, $path, false) ?? $request_value): $request_value));
+        $this->value = old($path, ($model ? (multi_dot_call($model, $path, false) ?? $request_value): $request_value));
 
-        if ($need_value == $value) {
+        $this->conditions($need_value, $params);
+    }
 
+    /**
+     * @param $need_value
+     * @param $params
+     */
+    protected function conditions($need_value, $params)
+    {
+        if ($need_value === static::NOT_NULL) {
+            $can = $this->value != null;
+        } else if ($need_value === static::NULL) {
+            $can = $this->value == null;
+        } else if ($need_value === static::TRUE) {
+            $can = !!$this->value === true;
+        } else if ($need_value === static::FALSE) {
+            $can = !!$this->value === false;
+        } else if (is_array($need_value) && isset($need_value["IN"])) {
+            $can = in_array($this->value, (array)$need_value["IN"]);
+        } else if (is_array($need_value) && isset($need_value["NOT_IN"])) {
+            $can = !in_array($this->value, (array)$need_value["NOT_IN"]);
+        } else {
+            $can = $need_value == $this->value;
+        }
+
+        if ($can) {
             $this->when($params);
         }
+    }
+
+    /**
+     * @param ...$select
+     * @return array
+     */
+    public static function IN(...$select)
+    {
+        return ["IN" => $select];
+    }
+
+    /**
+     * @param ...$select
+     * @return array
+     */
+    public static function NOT_IN(...$select)
+    {
+        return ["NOT_IN" => $select];
     }
 }
