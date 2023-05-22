e0e02d2813adbd246d329a05934cb4be795f8d53
xsaven <gms.sw.xsaven@gmail.com>
2023-05-22 18:40:34 +0300

    src/Components/GridRowComponent.php

--- src/Components/GridRowComponent.php
+++ src/Components/GridRowComponent.php
@@ -15,4 +15,18 @@ class GridRowComponent extends Component
     {
         // TODO: Implement mount() method.
     }
+
+    /**
+     * @return mixed|void
+     */
+    public function onRender()
+    {
+        $this->newExplainForce($this->delegates);
+        $this->newExplainForce($this->force_delegates);
+        if (!$this->iSelectModel && ($this->parent?->model ?? null)) {
+            $this->model($this->parent->model);
+        }
+        $this->mount();
+        $this->callRenderEvents();
+    }
 }
