8d23afdba3341e249cb33a21a05c808c63f0a10a
xsaven <gms.sw.xsaven@gmail.com>
2023-10-28 02:19:42 +0300

    src/Controllers/PermissionController.php

--- src/Controllers/PermissionController.php
+++ src/Controllers/PermissionController.php
@@ -92,27 +92,6 @@ class PermissionController extends Controller
                         ->default(1),
                     $form->ifEdit()->info_updated_at(),
                     $form->ifEdit()->info_created_at(),
-
-
-                    $form->dual_select('test0', 'test0')->options(['1' => '1', '2' => '2']),
-                    $form->checks('test1', 'test1')->options(['1' => '1', '2' => '2']),
-                    $form->select_tags('test10', 'test10')->options(['1' => '1', '2' => '2']),
-                    $form->ckeditor('test2', 'test2'),
-                    $form->divider(),
-                    $form->divider('1'),
-                    $form->divider(null, '2'),
-                    $form->divider(null, null, '3'),
-                    $form->codemirror('test3', 'test3'),
-                    $form->color('test4', 'test4'),
-                    $form->date('test5', 'test5'),
-                    $form->date_range('test6', 'test6'),
-                    $form->hidden('test7', 'test7')->value(123),
-                    $form->mdeditor('test8', 'test8'),
-                    $form->number('test9', 'test9')->min(0),
-                    $form->slider('test11', 'test11')->min(0),
-                    $form->textarea('test12', 'test12')->rows(10),
-                    $form->time('test13', 'test13'),
-                    $form->password('password', 'password')->confirm(),
                 ),
                 $card->card_body()->p(
                     Str::markdown(__('admin.permission_instruction'))
