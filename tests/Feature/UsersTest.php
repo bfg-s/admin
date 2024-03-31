0697633a7cdb3ff7107d13b781a1b1749d889eb9
xsaven <gms.sw.xsaven@gmail.com>
2024-03-31 19:03:05 +0300

    test

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -13,33 +13,34 @@ class UsersTest extends TestCase
 
         $this->browse(function (Browser $browser) {
             $browser->visitRoute('admin.profile')
-                ->assertSee('Admin');
-//                ->type('#input_password', 'admin2')
-//                ->type('#input_password_confirmation', 'admin2')
-//                ->click('button[data-click="submit"]')
-//                ->assertSee('Saved successfully!');
+                ->assertSee('Admin')
+                ->type('password', 'admin2')
+                ->type('password_confirmation', 'admin2')
+                ->click('[data-click="submit"]')
+                //->assertSee('Saved successfully!');
+                ->screenshot('test_change_admin_password');
         });
 
-//        $this->browse(function (Browser $browser) {
-//
-//            if($browser->element('[data-original-title="Logout"]')) {
-//                $browser->visitRoute('admin.profile.logout');
-//            }
-//
-//            $browser->visitRoute('admin.login')
-//                ->type('login', 'admin')
-//                ->type('password', 'admin2')
-//                ->press('Sign In')
-//                ->assertSee('admin@admin.com');
-//        });
-//
-//        $this->browse(function (Browser $browser) {
-//
-//            $browser->visitRoute('admin.profile')
-//                ->type('#input_password', 'admin')
-//                ->type('#input_password_confirmation', 'admin')
-//                ->click('button[data-click="submit"]')
-//                ->assertSee('Saved successfully!');
-//        });
+        $this->browse(function (Browser $browser) {
+
+            if($browser->element('[data-original-title="Logout"]')) {
+                $browser->visitRoute('admin.profile.logout');
+            }
+
+            $browser->visitRoute('admin.login')
+                ->type('login', 'admin')
+                ->type('password', 'admin2')
+                ->press('Sign In')
+                ->assertSee('admin@admin.com');
+        });
+
+        $this->browse(function (Browser $browser) {
+
+            $browser->visitRoute('admin.profile')
+                ->assertSee('Admin')
+                ->type('password', 'admin')
+                ->type('password_confirmation', 'admin')
+                ->click('button[data-click="submit"]');
+        });
     }
 }
