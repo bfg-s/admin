318dfb5ffd5898b5504b96ba28c9651384527772
xsaven <gms.sw.xsaven@gmail.com>
2025-02-15 00:41:20 +0200

    themes/lte/components/card.blade.php, themes/lte/components/model-table/footer.blade.php

--- themes/lte/components/card.blade.php
+++ themes/lte/components/card.blade.php
@@ -2,7 +2,7 @@
     @class(['card', 'card-outline', 'w-100', "card-{$type}"])
     @attributes($attributes)
 >
-    <div class="card-header" @reatime('card-header-' . $model_name)>
+    <div class="card-header" @realtime('card-header-' . $model_name)>
         @if($headerObj) {!! $headerObj !!} @endif
 
         <h3 class="card-title">
