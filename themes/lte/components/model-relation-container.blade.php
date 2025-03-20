4757414f4517fb4693e968f7c75aeb0cc19616d0
xsaven <gms.sw.xsaven@gmail.com>
2025-03-20 02:28:28 +0200

    themes/lte/components/model-relation-container.blade.php

--- themes/lte/components/model-relation-container.blade.php
+++ themes/lte/components/model-relation-container.blade.php
@@ -7,9 +7,9 @@
         <div class="card-header">
             @if($model_id)
                 <span>
-                    <string>ID:</string> {{ $model_id }}
-                    @if($created_at) 🔹 <string>Created at:</string> {{ beautiful_date_time($created_at) }} @endif
-                    @if($updated_at) 🔹 <string>Updated at:</string> {{ beautiful_date_time($updated_at) }} @endif
+                    <strong>ID:</strong> {{ $model_id }}
+                    @if($created_at) 🔹 <strong>Created at:</strong> {{ beautiful_date_time($created_at) }} @endif
+                    @if($updated_at) 🔹 <strong>Updated at:</strong> {{ beautiful_date_time($updated_at) }} @endif
                 </span>
             @endif
             <h3 class="card-title return_relation" style="display: none; margin-right: 5px">
